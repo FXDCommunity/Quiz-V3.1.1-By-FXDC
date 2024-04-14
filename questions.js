bb175a86620b04bf1c664cdba5a5b64cc1ea9435
FXDCommunity <166769846+FXDCommunity@users.noreply.github.com>
2024-04-14 00:08:19 -0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,77 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 26,
+        question: "Mario Bros Termasuk Game?",
+        answer: "Indie",
+        options: ["Pixel", "Indie", "3D", "2D"]
+    },
+    {
+        numb: 27,
+        question: "Bioskop Simulator Di Kembangkan Oleh?",
+        answer: "Andy Lukito",
+        options: ["Akhir Pekan Studio", "Andy Lukito", "Kajewdev", "Livander Gamedev"]
+    },
+    {
+        numb: 28,
+        question: "Kepanjangan Game BUSSID Adalah?",
+        answer: "Bus Simulator Indonesia",
+        options: ["Bus Simulator Indie", "Bus Simulator Indonesia", "Bus Simulator Index", "Bus Simulator India"]
+    },
+    {
+        numb: 29,
+        question: "Game Yang Sedang Di Kembangkan Andy Lukito Sekarang Ini Adalah?",
+        answer: "Warnet Life 2",
+        options: ["Bioskop Simulator", "Warnet Life", "Warnet Life 2", "Kantin Sekolah Simulator"]
+    },
+    {
+        numb: 30,
+        question: "Aku Si Peternak Lele DI Kembangkan Oleh?",
+        answer: "Kajewdev",
+        options: ["Andy Lukito", "Livander Gamedev", "Kajewdev", "Lainnya"]
+    },
+    {
+        numb: 31,
+        question: "Unreal Engine Yang Memiliki Grafis Tertinggi Adalah?",
+        answer: "Unreal Engine 5",
+        options: ["Unreal Engine 2", "Unreal Engne 3", "Unreal Engine 4", "Unreal Engine 5"]
+    },
+    {
+        numb: 32,
+        question: "PUBG Mobile Menggunkan Unreal Engine Ke Berapa?",
+        answer: "Unreal Engine 4",
+        options: ["Unreal Engine 1", "Unrea; Engine 2", "Unreal Engine 3", "Unreal Engine 4"]
+    },
+    {
+        numb: 33,
+        question: "Stardew Valley Di Buar Oleh Berapa Orang?",
+        answer: "1 Orang",
+        options: ["2 Orang", "1 Orang", "4 Orang", "3 Orang"]
+    },
+    {
+        numb: 34,
+        question: "Guardian Tales Rilis Pada?",
+        answer: "24 Febuari 2020",
+        options: ["23 Febuari 2020", "24 Febuari 2020", "25 Febuari 2020", "26 Febuari 2020"]
+    },
+    {
+        numb: 35,
+        question: "Mortal Kombat Shaolin Monks Rilis Pada?",
+        answer: "19 September 2005",
+        options: ["19 September 2005", "20 September 2005", "21 September 2005", "22 September 2005"]
+    },
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+    // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    //   {
+    //   numb: 6,
+    //   question: "Your Question is Here",
+    //   answer: "Correct answer of the question is here",
+    //   options: [
+    //     "Option 1",
+    //     "option 2",
+    //     "option 3",
+    //     "option 4"
+    //   ]
+    // },
+];
